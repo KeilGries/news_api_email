ea9ec3f90bd54da7cad1be93c2a5b7b44f51b93e
KG <KeilGriesLP@gmail.com>
2023-03-30 17:10:22 -0400

    Minor bug fixes.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+import os
 from send_email import send_email
 
 topic = 'tesla'
 
-api_key = 'd2992057f3b14c4ba469d54a25726661'
+api_key = os.getenv('NEWSAPIPASSWORD')
+
 url = 'https://newsapi.org/v2/everything?' \
       f'q={topic}&' \
       'from=2023-02-28&sortBy=publishedAt&apiKey=' \
@@ -25,7 +27,6 @@ for article in content['articles'][:20]:
             body = body + article['title'] + '\n' \
                    + article['description'] \
                    + '\n' + article['url'] + 2*'\n'
-
 
 body = body.encode('utf-8')
 send_email(message=body)
